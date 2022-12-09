61c4797a7964402e22485964a32d92d766edc513
gr8coda <gr8coda@gmail.com>
2022-12-09 10:36:41 +0000

    Created every methods to the expense controller

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class ExpenseController extends Controller
 {
-    //
+    public function index(Request $request) {
+
+    }
+
+    public function add(Request $request) {
+
+    }
+
+    public function edit(Request $request, $id) {
+
+    }
+
+    public function view(Request $request, $id) {
+
+    }
+
+    public function delete(Request $request, $id) {
+
+    }
+
+    public function duplicate(Request $request, $id) {
+
+    }
 }
